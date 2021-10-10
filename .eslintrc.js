831f8e39157427432eb5766ecf8293b30199822b
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2021-10-10 01:16:56 +0100

    eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,28 @@
 module.exports = {
-    env: {
-        jest: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}, // this loads <rootdir>/tsconfig.json to eslint
     },
-    extends: 'airbnb-base',
-    rules: {
-        'comma-dangle': 0,
-        'no-underscore-dangle': 0,
-        'no-param-reassign': 0,
-        'no-return-assign': 0,
-        camelcase: 0,
-    }
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'comma-dangle': 0,
+    'no-underscore-dangle': 0,
+    'no-param-reassign': 0,
+    'no-return-assign': 0,
+    camelcase: 0,
+    'import/extensions': 0,
+    'arrow-parens': 0,
+    'no-console': 0,
+  },
 };
